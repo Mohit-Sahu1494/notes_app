05a36c8e8f0a607cdcb049b2a63627a50c9bd370
Mohit Sahu <sahumohit1494@gmail.com>
2026-04-19 01:18:27 +0530

    Change API base URL to production environment

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "http://localhost:8000/api/v1",
+   baseURL: "https://notesapp-production-b757.up.railway.app/api/v1",
 //    baseURL: "https://amusing-cat-production.up.railway.app/api/v1/admin",
   withCredentials: true, 
 });
